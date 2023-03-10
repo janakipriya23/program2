29039f405b702f154f0208af3d2f7a094ff9bc96
janakipriya23 <127486140+janakipriya23@users.noreply.github.com>
2023-03-10 16:12:08 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,13 @@
+package com.testingautomation;
+
+import java.util.Scanner;
+
+public class program2 {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 20;
+        int sum = first + second;
+        System.out.println(first + " + " + second + " = "  + sum);
+
+    }
+}
